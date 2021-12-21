35a0bc6b5ddf8085f06db52340ec5e13ddb3deb3
Rui Gonçalves <ruippeixotog@gmail.com>
2021-12-21 16:06:18 +0000

    Clean up memory tests

--- src/test/scala/component/MemorySpec.scala
+++ src/test/scala/component/MemorySpec.scala
@@ -31,24 +31,24 @@ class MemorySpec extends util.BaseSpec {
       val set, reset = new Port
       val ((q, nq), comp) = buildComponent { implicit env => nandLatch(set, reset) }
 
-      def setInputs(state: SimState, s: Boolean, r: Boolean): Unit = {
+      def setInputs(s: Boolean, r: Boolean)(state: SimState): Unit = {
         state.schedule(0, PortChange(set, Some(s)))
         state.schedule(0, PortChange(reset, Some(r)))
       }
 
       runPlan(
         comp,
-        10 -> { st => st.get(q) must beNone },
-        20 -> { st => setInputs(st, true, true) },
-        30 -> { st => st.get(q) must beNone },
-        40 -> { st => setInputs(st, true, false) },
-        50 -> { st => st.get(q) must beSome(true) },
-        60 -> { st => setInputs(st, true, true) },
-        70 -> { st => st.get(q) must beSome(true) },
-        80 -> { st => setInputs(st, false, true) },
-        90 -> { st => st.get(q) must beSome(false) },
-        100 -> { st => setInputs(st, true, true) },
-        110 -> { st => st.get(q) must beSome(false) }
+        10 -> { _.get(q) must beNone },
+        20 -> setInputs(true, true),
+        30 -> { _.get(q) must beNone },
+        40 -> setInputs(true, false),
+        50 -> { _.get(q) must beSome(true) },
+        60 -> setInputs(true, true),
+        70 -> { _.get(q) must beSome(true) },
+        80 -> setInputs(false, true),
+        90 -> { _.get(q) must beSome(false) },
+        100 -> setInputs(true, true),
+        110 -> { _.get(q) must beSome(false) }
       )
     }
   }
@@ -96,24 +96,24 @@ class MemorySpec extends util.BaseSpec {
       val set, reset = new Port
       val ((q, nq), comp) = buildComponent { implicit env => latchClocked(set, reset, clock(100)) }
 
-      def setInputs(state: SimState, s: Boolean, r: Boolean) = {
+      def setInputs(s: Boolean, r: Boolean)(state: SimState) = {
         state.schedule(0, PortChange(set, Some(s)))
         state.schedule(0, PortChange(reset, Some(r)))
       }
 
       runPlan(
         comp,
-        50 -> { st => st.get(q) must beNone },
-        150 -> { st => setInputs(st, false, false) },
-        250 -> { st => st.get(q) must beNone },
-        350 -> { st => setInputs(st, true, false) },
-        375 -> { st => st.get(q) must beNone },
-        450 -> { st => st.get(q) must beSome(true) },
-        550 -> { st => setInputs(st, false, false) },
-        650 -> { st => st.get(q) must beSome(true) },
-        750 -> { st => setInputs(st, false, true) },
-        775 -> { st => st.get(q) must beSome(true) },
-        850 -> { st => st.get(q) must beSome(false) }
+        50 -> { _.get(q) must beNone },
+        150 -> setInputs(false, false),
+        250 -> { _.get(q) must beNone },
+        350 -> setInputs(true, false),
+        375 -> { _.get(q) must beNone },
+        450 -> { _.get(q) must beSome(true) },
+        550 -> setInputs(false, false),
+        650 -> { _.get(q) must beSome(true) },
+        750 -> setInputs(false, true),
+        775 -> { _.get(q) must beSome(true) },
+        850 -> { _.get(q) must beSome(false) }
       )
     }
   }
@@ -138,21 +138,21 @@ class MemorySpec extends util.BaseSpec {
       val in = new Port
       val ((q, nq), comp) = buildComponent { implicit env => dLatch(in, clock(100)) }
 
-      def setInputs(state: SimState, d: Boolean) = {
+      def setInput(d: Boolean)(state: SimState) = {
         state.schedule(0, PortChange(in, Some(d)))
       }
 
       runPlan(
         comp,
-        25 -> { st => st.get(q) must beNone },
-        50 -> { st => setInputs(st, true) },
-        75 -> { st => st.get(q) must beNone },
-        150 -> { st => st.get(q) must beNone },
-        250 -> { st => st.get(q) must beSome(true) },
+        25 -> { _.get(q) must beNone },
+        50 -> setInput(true),
+        75 -> { _.get(q) must beNone },
+        150 -> { _.get(q) must beNone },
+        250 -> { _.get(q) must beSome(true) },
 
-        300 -> { st => setInputs(st, false) },
-        350 -> { st => st.get(q) must beSome(true) },
-        450 -> { st => st.get(q) must beSome(false) }
+        300 -> setInput(false),
+        350 -> { _.get(q) must beSome(true) },
+        450 -> { _.get(q) must beSome(false) }
       )
     }
   }
