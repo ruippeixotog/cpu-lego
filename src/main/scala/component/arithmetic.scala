ff471a7a1dad312f4a621d20e85237e4ebbfe740
Rui Gonçalves <ruippeixotog@gmail.com>
2021-12-28 00:36:50 +0000

    Add documentation to arithmetic and control components

--- src/main/scala/component/arithmetic.scala
+++ src/main/scala/component/arithmetic.scala
@@ -3,12 +3,16 @@ package component
 import component.BuilderAPI._
 import core._
 
+/** A full adder (page 81).
+  */
 def fullAdder(in1: Port, in2: Port, in3: Port): Spec[(Port, Port)] = newSpec {
   val carry = multi(or)(and(in1, in2), and(in1, in3), and(in2, in3))
   val out = multi(xor)(in1, in2, in3)
   (out, carry)
 }
 
+/** A binary adder of `ins1.length` bits (page 82).
+  */
 def binaryAdder(ins1: Bus, ins2: Bus, carryIn: Port = Low): Spec[(Bus, Port)] = newSpec {
   val (outs, carry) = ins1.zip(ins2).foldLeft((List.empty[Port], carryIn)) { case ((prevOuts, prevCarry), (a, b)) =>
     val (out, carry) = fullAdder(a, b, prevCarry)
@@ -17,6 +21,8 @@ def binaryAdder(ins1: Bus, ins2: Bus, carryIn: Port = Low): Spec[(Bus, Port)] = 
   (outs.reverse.toVector, carry)
 }
 
+/** A 2's-complement adder-subtracter of `ins1.length` bits (page 86).
+  */
 def addSub(ins1: Bus, ins2: Bus, sub: Port): Spec[Bus] = newSpec {
   binaryAdder(ins1, ins2.map(xor(_, sub)), sub)._1
 }
--- src/main/scala/component/control.scala
+++ src/main/scala/component/control.scala
@@ -3,6 +3,8 @@ package component
 import component.BuilderAPI._
 import core._
 
+/** An `ins.length` to 2^`ins.length` decoder.
+  */
 def decoder(ins: Bus, enable: Port): Spec[Bus] = newSpec {
   def aux(ins0: Bus, enable0: Port): Bus = ins0 match {
     case rest :+ in => aux(rest, and(enable0, not(in))) ++ aux(rest, and(enable0, in))
@@ -11,7 +13,9 @@ def decoder(ins: Bus, enable: Port): Spec[Bus] = newSpec {
   aux(ins, enable)
 }
 
+/** An `ins.length` to 1 multiplexer. `ins.length` must be equal to 2^`sel.length`.
+  */
 def mux(ins: Bus, sel: Bus): Spec[Port] = newSpec {
-  assert(ins.length == (1 << sel.length), "Input and address bus sizes do not match")
+  assert(ins.length == 1 << sel.length, "Input and address bus sizes do not match")
   ins.zip(decoder(sel, High)).map(and).reduce(or)
 }
