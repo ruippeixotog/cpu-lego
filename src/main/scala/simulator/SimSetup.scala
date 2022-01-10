e1cd0639764ece43aa4b0bf66f25f7eaed679156
Rui Gonçalves <ruippeixotog@gmail.com>
2022-01-10 23:48:54 +0000

    Refactor SimSetup

--- src/main/scala/simulator/SimSetup.scala
+++ src/main/scala/simulator/SimSetup.scala
@@ -10,72 +10,60 @@ object SimSetup {
     val sim = Sim(c).set(High, true).set(Low, false)
 
     c.components.foldLeft(sim) {
-      case (sim, nand: NAND) => setupNand(sim, nand)
-      case (sim, ff: FlipFlop) => setupFlipFlop(sim, ff)
-      case (sim, clock: Clock) => setupClock(sim, clock)
-      case (sim, posEdge: PosEdge) => setupPosEdge(sim, posEdge)
-      case (sim, switch: Switch) => setupSwitch(sim, switch)
-    }
-  }
 
-  def setupNand(sim: Sim, nand: NAND): Sim = {
-    def propagate(s: Sim): Sim = {
-      val res = (s.get(nand.in1), s.get(nand.in2)) match {
-        case (Some(false), _) => Some(true)
-        case (_, Some(false)) => Some(true)
-        case (Some(true), Some(true)) => Some(false)
-        case _ => None
-      }
-      s.setAfter(GateDelay, nand.out, res)
-    }
-    sim.watch(nand.in1, propagate).watch(nand.in2, propagate)
-  }
+      case (sim, NAND(in1, in2, out)) =>
+        binaryOp(sim, in1, in2, out, GateDelay) {
+          case (Some(false), _) => Some(true)
+          case (_, Some(false)) => Some(true)
+          case (Some(true), Some(true)) => Some(false)
+          case _ => None
+        }
 
-  def setupFlipFlop(sim: Sim, ff: FlipFlop): Sim = {
-    def propagate(s: Sim): Sim = {
-      val res = (s.get(ff.set), s.get(ff.reset)) match {
-        case (Some(true), Some(false)) => Some(true)
-        case (Some(false), Some(true)) => Some(false)
-        case _ => None
-      }
-      res.fold(s) { v =>
-        s.setAfter(GateDelay, ff.q, v).setAfter(GateDelay, ff.nq, !v)
-      }
-    }
-    sim.watch(ff.set, propagate).watch(ff.reset, propagate)
-  }
-
-  def setupClock(sim: Sim, clock: Clock): Sim = {
-    sim
-      .set(clock.out, true)
-      .watch(clock.out, _.toggleAfter(clock.freq, clock.out))
-  }
-
-  def setupPosEdge(sim: Sim, posEdge: PosEdge): Sim = {
-    sim
-      .set(posEdge.out, false)
-      .watch(
-        posEdge.in,
-        st => {
-          st.get(posEdge.in) match {
-            case Some(true) =>
-              st.setAfter(GateDelay, posEdge.out, true)
-                .setAfter(GateDelay + PosEdgeDelay, posEdge.out, false)
-            case _ =>
-              st
+      case (sim, FlipFlop(set, reset, q, nq)) =>
+        def propagate(s: Sim): Sim = {
+          val res = (s.get(set), s.get(reset)) match {
+            case (Some(true), Some(false)) => Some(true)
+            case (Some(false), Some(true)) => Some(false)
+            case _ => None
+          }
+          res.fold(s) { v =>
+            s.setAfter(GateDelay, q, v).setAfter(GateDelay, nq, !v)
           }
         }
-      )
+        sim.watch(set, propagate).watch(reset, propagate)
+
+      case (sim, Clock(freq, out)) =>
+        sim.set(out, true).watch(out, _.toggleAfter(freq, out))
+
+      case (sim, PosEdge(in, out)) =>
+        sim
+          .set(out, false)
+          .watch(
+            in,
+            st =>
+              st.get(in) match {
+                case Some(true) =>
+                  st.setAfter(GateDelay, out, true).setAfter(GateDelay + PosEdgeDelay, out, false)
+                case _ =>
+                  st
+              }
+          )
+
+      case (sim, Switch(in, out, enable)) =>
+        binaryOp(sim, enable, in, out, 0) {
+          case (Some(true), v) => v
+          case _ => None
+        }
+    }
   }
 
-  def setupSwitch(sim: Sim, switch: Switch): Sim = {
-    def propagate(s: Sim): Sim = {
-      val res = (s.get(switch.enable), s.get(switch.in)) match {
-        case (Some(true), v) => v
-        case _ => None
-      }
-      s.set(switch.out, res)
-    }
-    sim.watch(switch.enable, propagate).watch(switch.in, propagate)
+  private def binaryOp(sim: Sim, port1: Port, port2: Port, out: Port, delay: Int)(
+      f: (Option[Boolean], Option[Boolean]) => Option[Boolean]
+  ): Sim = {
+
+    def propagate(s: Sim): Sim =
+      s.setAfter(delay, out, f(s.get(port1), s.get(port2)))
+
+    sim.watch(port1, propagate).watch(port2, propagate)
   }
 }
